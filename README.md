a3c45011da3279c79b0c8a9bbbd0df3f703cb569
Htet Myat Soe <74512999+hms-19@users.noreply.github.com>
2023-08-21 01:20:25 +0630

    final readme

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 When you install ,
 
 1. RUN npm install
-2. RUN npm start
+2. CHANGE development at .env file when you run in local
+3. RUN npm start
 
 
 ## Tech Stack
--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const PRODUCTION_URL = 'http://52.197.235.216/api/v1'
 const DEVELOPMENT_URL = 'http://127.0.0.1/api/v1'
+
 let baseUrl = process.env.REACT_ENV == 'development' ? DEVELOPMENT_URL : PRODUCTION_URL
 const instance = axios.create({
         baseURL: baseUrl,
